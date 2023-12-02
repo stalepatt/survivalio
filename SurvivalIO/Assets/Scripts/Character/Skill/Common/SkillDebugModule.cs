b6ee90fccbab47a5fa42e557dd4ef06736b78ddf
Olive You <cheeeedy@gmail.com>
2023-12-02 21:46:46 +0900

    [Feat] Implement Skill System

---
+++ SurvivalIO/Assets/Scripts/Character/Skill/Common/SkillDebugModule.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+public class SkillDebugModule : MonoBehaviour
+{
+    public Bullet Bullet;
+    public Skill Skill;
+    public Data SkillData;
+    [Serializable]
+    public struct Data
+    {
+        public int ID;
+        public string Name;
+        public int Level;
+        public float Damage;
+        public float Speed;
+    }
+
+    public void Start()
+    {
+        Skill = GetComponent<Skill>();
+        SkillData data = Skill.GetCurrentSkillLevelData();
+        SkillData.ID = data.ID;
+        SkillData.Name = data.Name;
+        SkillData.Level = data.Level;
+        SkillData.Damage = data.BulletDamage;
+        SkillData.Speed = data.Speed;
+    }
+
+    public void Update()
+    {
+
+    }
+}
---
+++ SurvivalIO/Assets/Scripts/Character/Skill/Defender.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class Defender : Skill
+{
+    private SkillData _data;
+    private float[] _angles;
+
+    public override void Attack()
+    {
+        AttackTask().Forget();
+    }
+
+    public async UniTaskVoid AttackTask()
+    {
+        while (Managers.SceneManager.CurrentSceneType == Define.Scene.InGame)
+        {
+            Fire();
+            await UniTask.Delay(TimeSpan.FromSeconds(_data.AttackInterval));
+        }
+    }
+
+    private const float DISTANCE_FROM_PLAYER = 1.5f;
+    protected override void Fire()
+    {
+        _data = GetCurrentSkillLevelData();
+
+        Utils.GetAngleFromCircleDivide(_data.ProjectileCount, out _angles);
+
+        for (int bulletCount = 0; bulletCount < _data.ProjectileCount; ++bulletCount)
+        {
+            Bullet projectile = Managers.PoolManager.BulletPool.Get();
+            projectile.Init(this.transform, _data);
+            SetProjectilePosition(projectile.transform, bulletCount);
+        }
+
+        DefenderPatternTask().Forget();
+    }
+
+    private const float DEFAULT_SKILL_SPEED = 200f;
+    private readonly Vector3 ROTATE_DIRECTION = Vector3.forward;
+    public async UniTaskVoid DefenderPatternTask()
+    {
+        GameObject bullet = transform.GetChild(0).gameObject;
+        while (bullet.activeSelf)
+        {
+            float speed = DEFAULT_SKILL_SPEED * _data.Speed;
+            transform.Rotate(ROTATE_DIRECTION * speed * Time.deltaTime);
+
+            await UniTask.Yield();
+        }
+    }
+    private void SetProjectilePosition(Transform projectile, int bulletCount)
+    {
+        projectile.Rotate(Vector3.forward * _angles[bulletCount]);
+        projectile.Translate(projectile.transform.up * DISTANCE_FROM_PLAYER, Space.World);
+    }
+
+
+}
